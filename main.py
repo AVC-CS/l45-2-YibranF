2d1b5ef4826eb801f536dc9406252b8234298399
YibranF <yibranf109@gmail.com>
2024-10-10 19:40:43 -0700

    L45-2

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def main():
     total = 0
     numbers = []
     while total <= 100:
-            num = random.randint(0,100)
+            num = random.randint(1,20)
             numbers.append(num)
             print(num, end =' ')
             total += num
     print(f"\nTotal sum: {total}")
+    sum_less_than_100 = sum(num for num in numbers if num < 100)
+    print(f"Sum of random numbers less than 100: {sum_less_than_100}")
 
     ########################################
     # Do not delete the return statement
